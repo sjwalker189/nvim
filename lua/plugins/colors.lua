1cf8194d45a875eb5e53747ab3518d0b0845d922
Sam Walker <sam.walker@aviatnet.com>
2025-07-27 13:47:29 +1200

    Use github light color scheme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,12 +1,37 @@
 return {
   {
-    'lunacookies/vim-colors-xcode',
+
+    'projekt0n/github-nvim-theme',
     lazy = false,
     priority = 1000,
     init = function()
+      local palette = require('github-theme.palette').load 'github_light_default'
+      require('github-theme').setup {
+        options = {
+          transparent = true,
+          hide_nc_statusline = false,
+          hide_end_of_buffer = false,
+          -- inverse = {
+          --   match_paren = true,
+          --   visual = true,
+          --   search = true,
+          -- },
+        },
+        groups = {
+          all = {
+            ['@label.jsonc'] = { link = '@label.json' },
+          },
+          github_light_default = {
+            StatusLine = { bg = palette.bg1 },
+          },
+        },
+      }
+
       vim.o.background = 'dark'
       vim.opt.termguicolors = true
-      vim.cmd.colorscheme 'xcodelight'
+      vim.cmd.colorscheme 'github_light_default'
+
+      vim.api.nvim_set_hl(0, 'SnapBorder', { fg = '#cccccc' })
 
       local hl_groups = {
         'DiagnosticUnderlineError',
